a41332dfbe2737ca324a96bc0933058be1f8bf17
Himangshu Choudhury <himangshu610@gmail.com>
2024-07-07 20:37:22 +0530

    temp branch

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 print("Hello")
+<<<<<<< HEAD
 print("This is the change")
 print("Just another line of code")
 print({x:x*x for x in range(3)})
+=======
+print("This is the change")
+>>>>>>> parent of 182a67b (new module user added)
